935b637ae8bb72a6976fd9bdc7cb2cf7f3cb1b5a
Milan GBC <154081416+mcobanov-gbc@users.noreply.github.com>
2024-12-13 09:04:10 -0500

    Add files via upload

---
+++ scripts/external.js
@@ -0,0 +1,11 @@
+// window.alert("good morning milan");
+
+var today = new Date();
+// window.alert(today);
+
+var year = today.getFullYear();
+// window.alert("It is year " + year);
+
+
+var el = document.getElementById("year");
+el.innerHTML = year;
